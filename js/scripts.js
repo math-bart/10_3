508ca0c7cabaec40135787eb1c703f3a9d958400
Bartosz Wojtaś <el.klides@op.pl>
2018-02-12 22:38:38 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
+var a = prompt('podaj szerokość obrazu');
+var carousel = $("#carousel");
+var ul = $("ul");
+var li = $("li");
+var img = $("img");
+var controls = $(".carousel-controls");
+var right = $(".carousel-controls-right");
+var pointers = $(".pointers");
+carousel.css("width", a);
+carousel.css("height", a * 0.65);
+ul.css("width", a * 5);
+ul.css("height", a * 0.65);
+li.css("width", a);
+li.css("height", a * 0.65);
+img.css("width", a);
+img.css("height", a * 0.65);
+// controls.css("lineHeight", a * 0.65);  //ta deklaracja nie działa, jak ją odkomentuję to znikają mi kontrolki:(
+controls.css("height", a * 0.65);
+right.css("left", a - 50);
+pointers.css("top", a * 0.65 - 35);
+pointers.css("paddingLeft", (a - 100) / 2);
 $(function(){
   var carouselList = $("#carousel ul");
   var carouselPointers = $("#carousel .pointers");
   var interval = setInterval(changeSlide, 3000);
   function changeSlide() {
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    carouselList.animate({'marginLeft':-a}, 500, moveFirstSlide);
     carouselPointers.animate({'marginLeft':20}, 500, moveFirstPointer);
   }
   function moveFirstSlide() {
@@ -22,7 +43,7 @@ $(function(){
   var firstItem = carouselList.find("li:first");
   var lastItem = carouselList.find("li:last");
     firstItem.before(lastItem);
-    carouselList.css({marginLeft:-400});
+    carouselList.css({marginLeft:-a});
     carouselList.animate({'marginLeft':0}, 500);
     carouselPointers.animate({'marginLeft':-20}, 500, moveLastPointer);
   }
